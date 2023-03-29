49323b7e68b01152cc1ac626c685a83ece24d6c1
Kawaeee <kawaekc@gmail.com>
2023-03-29 14:30:36 +0700

    🍱 add C3-Fibonacci, C3-Monads

---
+++ exercises/C3-Fibonacci/src/main/scala/Fibonacci/Fibonacci.scala
@@ -0,0 +1,47 @@
+object Fibonacci{
+
+  def fib1(f0 : Int, f1 : Int, n : Int): Int = {
+    var i = 1
+    var a = f0
+    var b = f1
+    var sum = 0
+    while(i<n){
+     sum = a+b
+     a = b
+     b = sum
+     i += 1
+    }
+    sum
+  }
+
+  def fib2(f0 : Int, f1 : Int, n : Int): Int = {
+    if(n==0) f0
+    else if(n==1) f1
+    else fib2(f0,f1,n-1) + fib2(f0,f1,n-2)
+  }
+  
+  @annotation.tailrec
+  def fib3(f0 : Int, f1 : Int, n : Int): Int = {
+    if(n==0) f0
+    else if(n==1) f1
+    else fib3(f1,f0+f1,n-1)
+  }
+  
+  def fib4(f0 : Int, f1 : Int, n : Int): Int = {
+    def fib(f0 : Int,f1 : Int): Stream[Int] = f0 #:: fib(f1,f0+f1)
+    fib(f0,f1).take(n+1).toList.last
+  }
+
+
+  def main(args: Array[String]) : Unit = {
+    val f0 = if (args.length == 3) args(0).toInt else 5
+    val f1 = if (args.length == 3) args(1).toInt else 8
+    val n = if (args.length == 3) args(2).toInt else 10
+    printf(s"fib1(1,2,3): %d \n", fib1(1,2,3))
+    printf(s"fib1($f0,$f1,$n): %d \n", fib1(f0,f1,n))
+    printf(s"fib2($f0,$f1,$n): %d \n", fib2(f0,f1,n))
+    printf(s"fib3($f0,$f1,$n): %d \n", fib3(f0,f1,n))
+    printf(s"fib4($f0,$f1,$n): %d \n", fib4(f0,f1,n))
+  }
+
+}
---
+++ exercises/C3-Monads/src/main/scala/monads/Monads.scala
@@ -0,0 +1,15 @@
+import scala.collection.GenSeq
+
+object Monads{
+
+  def weightedAverage(a : GenSeq[Double], w : GenSeq[Double]): Double  =  a.zip(w).map(t => t match { case(x,y) => x*y } ).sum/w.sum
+
+  def concatenate(c : GenSeq[Char]): String = c.map(ci => ci.toString).reduce(_+_)
+
+  def main(args: Array[String]) : Unit = {
+    printf("weightedAverage(List(14.424, 14.421, 14.417, 14.413, 14.41),List(3058.0, 8826.0, 56705.0, 30657.0, 12984.0)): %f \n", weightedAverage(List(14.424, 14.421, 14.417, 14.413, 14.41),List(3058.0, 8826.0, 56705.0, 30657.0, 12984.0)))
+    printf("weightedAverage(List(1.0, 2.0, 3.0, 4.0, 5.0),List(2.0, 8.0, 50.0, 30.0, 10.0)): %f \n", weightedAverage(List(1.0, 2.0, 3.0, 4.0, 5.0),List(2.0, 8.0, 50.0, 30.0, 10.0)))
+    printf("concatenate(List('h','e','l','l','o')): %s \n", concatenate(List('h','e','l','l','o')))
+    printf("concatenate(List('s','c','a','l','a','_','d','s')): %s \n", concatenate(List('s','c','a','l','a','_','d','s')))
+  }
+}
